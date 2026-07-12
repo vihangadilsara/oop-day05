1e783ae4fe912acfee329c953842071d02d6483e
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-12 11:45:18 +0530

    Refactor Stack class into List class and update Demo for list operations

--- Demo.java
+++ Demo.java
@@ -4,22 +4,38 @@ class Node{
 	Node next;
 	Node(int data){this.data=data;}
 }
-class Stack{
-	private Node top;
-	public void push(int data){
+class List{
+	private Node first;
+
+	public void add(int data){
 		Node n1=new Node(data);
-		n1.next=top;
-		top=n1;
+		//find the last node
+		if(isEmpty()){
+			first=n1;
+		}else{
+			Node lastNode=first;
+			while(lastNode.next!=null){
+				lastNode=lastNode.next;
+			}
+			lastNode.next=n1;
+		}
+
+	}
+	public void add(int index, int data){
 		
 	}
-	public void pop(){
-		if(top!=null){
-			top=top.next;
-		}
+	public void remove(int index){
+		//	
 	}
-	public void printStack(){
+
+
+	public int get(int index){
+		//
+		return -1;
+	}
+	public void printList(){
 		System.out.print("[");
-		Node temp=top;
+		Node temp=first;
 		while(temp!=null){
 			System.out.print(temp.data+", ");
 			temp=temp.next;
@@ -27,7 +43,7 @@ class Stack{
 		System.out.println(isEmpty() ?"empty]":"\b\b]");
 	}
 	public int size(){
-		Node temp=top;
+		Node temp=first;
 		int count=0;
 		while(temp!=null){
 			count++;
@@ -36,13 +52,13 @@ class Stack{
 		return count;
 	}
 	public boolean isEmpty(){
-		return top==null;
+		return first==null;
 	}
 	public void clear(){
-		top=null;
+		first=null;
 	}
 	public int search(int data){
-		Node temp=top;
+		Node temp=first;
 		int index=0;
 		while(temp!=null){
 			if(temp.data==data){
@@ -53,21 +69,29 @@ class Stack{
 		}
 		return -1;
 	}
+	public int[] toArray(){
+		int[] tempDataArray=new int[size()];
+		Node temp=first;
+		for (int i = 0; i < tempDataArray.length; i++)	{
+			tempDataArray[i]=temp.data;
+			temp=temp.next;
+		}
+		return tempDataArray;
+	}
 }
 
 
 
 class Demo{
 	public static void main(String args[]){
-		Stack s1=new Stack();
-		s1.push(100);
-		s1.push(200);
-		s1.push(300);
-		s1.push(400);
-		s1.push(500);
-		s1.printStack(); //[500, 400, 300, 200, 100]
+		List intList=new List();
+		intList.add(100); //add(data)-->Insertion order[Queue]
+		intList.add(200);
+		intList.add(300);
+		intList.add(400);
+		intList.add(500);
+		intList.printList();//[100, 200, 300, 400, 500]
 		
-		int[] dataArray=s1.toArray();
-		System.out.println(Arrays.toString(dataArray));//[500, 400, 300, 200, 100]
+		intList.add(2,250);//[100, 200, 250, 300, 400, 500]
 	}
 }
