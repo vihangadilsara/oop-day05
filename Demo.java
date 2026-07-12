e15bb9b54ddac2f1b00e591c0c664e268e2709f9
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-12 10:31:30 +0530

    Refactor Stack class methods to improve functionality and add stack state printing

--- Demo.java
+++ Demo.java
@@ -6,30 +6,44 @@ class Node{
 class Stack{
 	private Node top;
 	public void push(int data){
-		//
+		Node n1=new Node(data);
+		n1.next=top;
+		top=n1;
+		
 	}
 	public void pop(){
-		//	
+		if(top!=null){
+			top=top.next;
+		}
 	}
 	public void printStack(){
-		//
+		System.out.print("[");
+		Node temp=top;
+		while(temp!=null){
+			System.out.print(temp.data+", ");
+			temp=temp.next;
+		}
+		System.out.println("\b\b]");
 	}
 }
-
-
-
-
 class Demo{
 	public static void main(String args[]){
 		Stack s1=new Stack();
+		s1.printStack();//[empty]
+		System.out.println("Size of the stack : "+s1.size()); //0
+		System.out.println("Stack is empty    : "+s1.isEmpty());//true
 		s1.push(100);
 		s1.push(200);
 		s1.push(300);
 		s1.push(400);
 		s1.push(500);
 		s1.printStack(); //[500, 400, 300, 200, 100]
+		System.out.println("Size of the stack : "+s1.size()); //5
+		System.out.println("Stack is empty    : "+s1.isEmpty());//false
 		
-		s1.pop();
-		s1.printStack(); //[400, 300, 200, 100]
+		s1.clear();
+		s1.printStack();//[empty]
+		System.out.println("Size of the stack : "+s1.size()); //0
+		System.out.println("Stack is empty    : "+s1.isEmpty());//true
 	}
 }
