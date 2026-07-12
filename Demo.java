ca1e6a7a5a216480d0d2651135e2c9cc351aaaec
vihangadilsara <vdilsara30@gmail.com.com>
2026-07-12 09:29:04 +0530

    Refactor Demo.java to implement Node class and linked list data printing

--- Demo.java
+++ Demo.java
@@ -1,26 +1,29 @@
-class Marks{
-	int prfMarks;
-	int dbmsMarks;
-	Marks(int prfMarks, int dbmsMarks){
-		this.prfMarks=prfMarks;
-		this.dbmsMarks=dbmsMarks;
-	} 
-}
-class Student{
-	String id;
-	String name;
-	Marks studentMarks;
-	
-	Student(){
-	}
-	Student(String id, String name){
-		this.id=id;
-		this.name=name;
-	} 
+class Node{
+	int data;
+	Node next;
+	Node(int data){this.data=data;}
 }
 class Demo{
 	public static void main(String args[]){
-		Student s1=new Student("S0001","Nimal");	
-		s1.studentMarks=new Marks(67,78);
+		Node n1=new Node(100);
+		n1.next=new Node(200);
+		n1.next.next=new Node(300);
+		n1.next.next.next=new Node(400);
+		n1.next.next.next.next=new Node(500);
+		
+		//prints data- [100, 200, 300, 400, 500] using a while loop
+		System.out.print("[");
+		
+		System.out.print(n1.data+", "); 
+		n1=n1.next;
+		
+		System.out.print(n1.data+", ");
+		n1=n1.next;
+		
+		while(n1!=null){
+			System.out.print(n1.data+", "); 
+			n1=n1.next;
+		}
+		System.out.println("\b\b]");
 	}
 }
